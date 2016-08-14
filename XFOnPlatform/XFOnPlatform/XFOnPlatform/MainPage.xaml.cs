6ca15dd046eb775dcdc7e43536c0fa089a871c45
李進興 Vulcan <vulcan@miniasp.com>
2016-08-14 11:52:52 +0800

    OnPlatform的不同用法 XAML & C#

---
+++ XFOnPlatform/XFOnPlatform/XFOnPlatform/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFOnPlatform
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Device.OnPlatform(
+                    Android: () =>
+                    {
+                        lblMessage.Text = "我在 Android 系統下";
+                    },
+                    iOS: () =>
+                    {
+                        lblMessage.Text = "我在 iOS 系統下";
+                    },
+                    WinPhone: () =>
+                    {
+                        lblMessage.Text = "我在 Windows 系統下";
+                    });
+        }
+    }
+}
