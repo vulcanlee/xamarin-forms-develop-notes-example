1df12bd94217f0f94a66d49868f1c55501240677
李進興 Vulcan <vulcan@miniasp.com>
2016-08-16 15:24:15 +0800

    Xamarin.Forms 的應用程式生命週期

---
+++ XFLifeCycle/XFLifeCycle/XFLifeCycle/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XFLifeCycle
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new XFLifeCycle.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            Debug.WriteLine("OnStart");
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Debug.WriteLine("OnSleep");
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+            Debug.WriteLine("OnResume");
+        }
+    }
+}
