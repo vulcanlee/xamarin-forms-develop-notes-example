2d81bef2bff181cf88881d37f5c5451aabe2e25a
李進興 Vulcan <vulcan@miniasp.com>
2016-08-17 12:27:32 +0800

    Xamarin.Forms 使用者控制項

---
+++ XFUserControl/XFUserControl/XFUserControl/MyControl.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XFUserControl
+{
+    public partial class MyControl : Grid
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
