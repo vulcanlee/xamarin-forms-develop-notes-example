4b6b826efbe4ccfa2b1c9bfbecd70ce7455cd4d8
李進興 Vulcan <vulcan@miniasp.com>
2016-08-19 10:28:21 +0800

    Xamarin.Forms 警告與動作表對話窗

---
+++ XFAlert/XFAlert/XFAlert/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,68 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFAlert.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        public IPageDialogService _dialogService { get; set; }
+        public DelegateCommand PrismAlertCommand { get; set; }
+        public DelegateCommand PrismActionSheetCommand { get; set; }
+
+        #region PrismAlert輸出結果
+        private string _PrismAlert輸出結果;
+        public string PrismAlert輸出結果
+        {
+            get { return _PrismAlert輸出結果; }
+            set { SetProperty(ref _PrismAlert輸出結果, value); }
+        }
+        #endregion
+
+        #region PrismActionSheet輸出結果
+        private string _PrismActionSheet輸出結果;
+        public string PrismActionSheet輸出結果
+        {
+            get { return _PrismActionSheet輸出結果; }
+            set { SetProperty(ref _PrismActionSheet輸出結果, value); }
+        }
+        #endregion
+
+        public MainPageViewModel(IPageDialogService dialogService)
+        {
+            _dialogService = dialogService;
+
+            PrismAlertCommand = new DelegateCommand(PrismAlert);
+            PrismActionSheetCommand = new DelegateCommand(PrismActionSheet);
+
+            PrismAlert輸出結果 = "Prism 選擇結果";
+            PrismActionSheet輸出結果 = "Prism 動作表單 選擇結果";
+        }
+
+        private async void PrismAlert()
+        {
+            await _dialogService.DisplayAlertAsync("警告", "您確定要繼續執行嗎?", "確定");
+            bool 選擇結果 =await _dialogService.DisplayAlertAsync("通知", "您要開始下載這個檔案嗎?", "是", "否");
+            PrismAlert輸出結果 = 選擇結果.ToString();
+        }
+
+        private async void PrismActionSheet()
+        {
+            var 選擇結果 = await _dialogService.DisplayActionSheetAsync("請選擇您要分享的目的應用程式?", "取消", "Google+", "Email", "Twitter", "Facebook");
+            PrismActionSheet輸出結果 = 選擇結果;
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+        }
+    }
+}
---
+++ XFAlert/XFAlert/XFAlert/Views/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Xamarin.Forms;
+
+namespace XFAlert.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnAlert.Clicked += async (s, e) =>
+              {
+                  await DisplayAlert("警告", "您確定要繼續執行嗎?", "確定");
+                  bool 選擇結果 = await DisplayAlert("通知", "您要開始下載這個檔案嗎?", "是", "否");
+                  lblXF選擇結果.Text = 選擇結果.ToString();
+              };
+            btnActionSheet.Clicked += async (s, e) =>
+              {
+                  var 選擇結果 = await DisplayActionSheet("請選擇您要分享的目的應用程式?", "取消", "Google+", "Email", "Twitter", "Facebook");
+                  lblXF動作表單選擇結果.Text = 選擇結果;
+              };
+        }
+
+    }
+}
