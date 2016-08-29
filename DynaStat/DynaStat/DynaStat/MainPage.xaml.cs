3203114d4cb8f4a962b5adc040b49afe01c0efee
李進興 Vulcan <vulcan@miniasp.com>
2016-08-29 23:06:16 +0800

    XAML 靜態與動態資源的使用

---
+++ DynaStat/DynaStat/DynaStat/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DynaStat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnDynamic.Clicked += (s, e) =>
+              {
+                  this.Resources["backgroundColor"] = Color.White;
+                  this.Resources["textColor"] = Color.Black;
+                  this.Resources["fontSize"] = 30;
+                  btnDynamic.Text = $"變更動態資源 " + this.Resources.Count.ToString();
+              };
+            btnGlobalDynamic.Clicked += (s, e) =>
+              {
+                  App.Current.Resources["GlobalbackgroundColor"] = Color.White;
+                  App.Current.Resources["GlobaltextColor"] = Color.Black;
+                  App.Current.Resources["GlobalfontSize"] = 30;
+                  btnGlobalDynamic.Text = $"變更全域動態資源 "+ App.Current.Resources.Count.ToString();
+              };
+            btnStatic.Clicked += (s, e) =>
+              {
+                  PubStat.文字顏色 = Color.Blue;
+                  PubStat.背景顏色 = Color.Pink;
+                  PubStat.文字尺寸 = 30;
+              };
+            btnReset.Clicked += (s, e) =>
+              {
+                  App.Current.MainPage = new MainPage();
+              };
+        }
+    }
+}
---
+++ DynaStat/DynaStat/DynaStat/PubStat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DynaStat
+{
+    public class PubStat
+    {
+        public static Color 文字顏色 { get; set; } = Color.Red;
+        public static Color 背景顏色 { get; set; } = Color.Yellow;
+        public static double 文字尺寸 { get; set; } = 20;
+    }
+}
