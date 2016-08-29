c2000e07eb937848c39a1cbb47c38548277093a4
李進興 Vulcan <vulcan@miniasp.com>
2016-08-29 20:17:32 +0800

    XAML 的 樣式 style 應用

---
+++ MergedDict/MergedDict/MergedDict/Main2Page.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace MergedDict
+{
+    public partial class Main2Page : ContentPage
+    {
+        public Main2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MergedDict/MergedDict/MergedDict/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MergedDict
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.btn.Clicked += (s, e) =>
+              {
+                  Navigation.PushAsync(new Main2Page());
+              };
+        }
+    }
+}
