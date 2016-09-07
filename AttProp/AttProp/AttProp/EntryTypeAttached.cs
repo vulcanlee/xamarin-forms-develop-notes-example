faf99fd2ba765020dabc9e4efbb9c9ed3b22ea18
李進興 Vulcan <vulcan@miniasp.com>
2016-09-07 12:40:14 +0800

    自訂附加屬性 Attached Property

---
+++ AttProp/AttProp/AttProp/EntryTypeAttached.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AttProp
+{
+    public class EntryTypeAttached
+    {
+        public static readonly BindableProperty EntryTypeProperty =
+               BindableProperty.CreateAttached(
+                   propertyName: "EntryType",
+                   returnType: typeof(string),
+                   declaringType: typeof(Entry),
+                   defaultValue: null,
+                   defaultBindingMode: BindingMode.OneWay,
+                   validateValue: null,
+                   propertyChanged: OnEntryTypeChanged);
+
+        private static void OnEntryTypeChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var fooEntry = bindable as Entry;
+            if (fooEntry == null)
+                return;
+
+            var foooldValue = (oldValue as string)?.ToLower();
+            var foonewValue = (newValue as string)?.ToLower();
+            if(foonewValue == null)
+            {
+                return;
+            }
+            switch (foonewValue)
+            {
+                case "None":
+                    break;
+                case "email":
+                    fooEntry.SetValue(Entry.PlaceholderProperty, "請輸入電子郵件");
+                    fooEntry.Keyboard = Keyboard.Email;
+                    fooEntry.FontSize = 20;
+                    break;
+                case "phone":
+                    fooEntry.SetValue(Entry.PlaceholderProperty, "請輸入電話號碼");
+                    fooEntry.Keyboard = Keyboard.Telephone;
+                    fooEntry.FontSize = 20;
+                    break;
+                case "number":
+                    fooEntry.SetValue(Entry.PlaceholderProperty, "請輸入數值");
+                    fooEntry.Keyboard = Keyboard.Numeric;
+                    fooEntry.FontSize = 20;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Helper methods for attached bindable property. 
+        public static void SetEntryType(BindableObject bindable, string entryType)
+        {
+            bindable.SetValue(EntryTypeProperty, entryType);
+        }
+        public static string GetEntryType(BindableObject bindable)
+        {
+            return (string)bindable.GetValue(EntryTypeProperty);
+        }
+    }
+}
