57c3f146cb7dfc59604612545a7e87cc8dac9e3c
李進興 Vulcan <vulcan@miniasp.com>
2016-09-07 12:24:42 +0800

    自訂綁定屬性 Bindable Property

---
+++ BindProp/BindProp/BindProp/DoggyEntry.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindProp
+{
+    public class DoggyEntry : Entry
+    {
+        // https://developer.xamarin.com/guides/xamarin-forms/xaml/bindable-properties/
+        public static readonly BindableProperty EntryTypeProperty =
+            BindableProperty.Create("EntryType", // 屬性名稱 
+                typeof(string), // 回傳類型 
+                typeof(DoggyEntry), // 宣告類型 
+                "None", // 預設值 
+                propertyChanged: OnEntryTypeChanged);
+
+        private static void OnEntryTypeChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var fooEntry = bindable as DoggyEntry;
+            var foooldValue = (oldValue as string).ToLower();
+            var foonewValue = (newValue as string).ToLower();
+
+            switch (foonewValue)
+            {
+                case "None":
+                    break;
+                case "email":
+                    fooEntry.SetValue(PlaceholderProperty, "請輸入電子郵件");
+                    fooEntry.Keyboard = Keyboard.Email;
+                    fooEntry.FontSize = 20;
+                    break;
+                case "phone":
+                    fooEntry.SetValue(PlaceholderProperty, "請輸入電話號碼");
+                    fooEntry.Keyboard = Keyboard.Telephone;
+                    fooEntry.FontSize = 20;
+                    break;
+                case "number":
+                    fooEntry.SetValue(PlaceholderProperty, "請輸入數值");
+                    fooEntry.Keyboard = Keyboard.Numeric;
+                    fooEntry.FontSize = 20;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public DoggyEntry()
+        {
+
+        }
+
+        public string EntryType
+        {
+            set
+            {
+                SetValue(EntryTypeProperty, value);
+            }
+            get
+            {
+                return (string)GetValue(EntryTypeProperty);
+            }
+        }
+    }
+}
---
+++ BindProp/BindProp/BindProp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BindProp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
