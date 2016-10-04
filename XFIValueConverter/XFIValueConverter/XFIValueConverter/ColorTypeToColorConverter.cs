c3fa8c53c87195c002fbb3a38683dbadfe8c80db
李進興 Vulcan <vulcan@miniasp.com>
2016-10-04 16:18:21 +0800

    數值轉換器 Value Converter

---
+++ XFIValueConverter/XFIValueConverter/XFIValueConverter/ColorTypeToColorConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFIValueConverter
+{
+    class ColorTypeToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var foo = value as string;
+            if (foo == "1")
+            {
+                return Color.Red;
+            }
+            else if (foo == "2")
+            {
+                return Color.Blue;
+            }
+            else if (foo == "3")
+            {
+                return Color.Green;
+            }
+            else
+            {
+                return Color.Black;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ XFIValueConverter/XFIValueConverter/XFIValueConverter/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFIValueConverter.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region ColorType
+        private string colorType;
+        /// <summary>
+        /// ColorType
+        /// </summary>
+        public string ColorType
+        {
+            get { return this.colorType; }
+            set { this.SetProperty(ref this.colorType, value); }
+        }
+        #endregion
+
+        public MainPageViewModel()
+        {
+
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
