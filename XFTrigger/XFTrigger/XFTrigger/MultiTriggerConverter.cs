8866142cc9a52bff6506bf068938a2f9e96f29c1
李進興 Vulcan <vulcan@miniasp.com>
2016-10-04 23:24:16 +0800

    練習：觸發 Trigger

---
+++ XFTrigger/XFTrigger/XFTrigger/MultiTriggerConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFTrigger
+{
+    class MultiTriggerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            if ((int)value > 0)
+                return true;    // data has been entered
+            else
+                return false;   // input is empty
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ XFTrigger/XFTrigger/XFTrigger/NumericValidationTriggerAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFTrigger
+{
+    class NumericValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            double result;
+            bool isValid = Double.TryParse(entry.Text, out result);
+            entry.TextColor = isValid ? Color.Default : Color.Red;
+            entry.Scale = 1.4;
+        }
+    }
+}
