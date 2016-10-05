8e4250f8f443f54a9be03385c46a8c8c20d8e9ea
李進興 Vulcan <vulcan@miniasp.com>
2016-10-05 23:49:00 +0800

    動畫 Animations

---
+++ XFAnimation/XFAnimation/XFAnimation/ViewModels/NewPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFAnimation.ViewModels
+{
+    public class NewPageViewModel : BindableBase
+    {
+        public NewPageViewModel()
+        {
+
+        }
+    }
+}
---
+++ XFAnimation/XFAnimation/XFAnimation/Views/NewPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFAnimation.Views
+{
+    public partial class NewPage : ContentPage
+    {
+        public NewPage()
+        {
+            InitializeComponent();
+        }
+        protected async override void OnAppearing()
+        {
+            await 頁面攤開來();
+            base.OnAppearing();
+        }
+
+        public async Task 頁面收起來()
+        {
+            this.AnchorX = 0;
+            this.RotationY = 0;
+            await this.RotateYTo(-180, 2000);
+            this.AnchorX = 0.5;
+            this.RotationY = 0;
+        }
+        public async Task 頁面攤開來()
+        {
+            this.AnchorX = 0;
+            this.RotationY = -180;
+            await this.RotateYTo(0, 2000);
+            this.AnchorX = 0.5;
+            this.RotationY = 0;
+        }
+    }
+}
