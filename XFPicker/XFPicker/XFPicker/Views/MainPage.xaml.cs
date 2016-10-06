e3731e6311eb7d9a738d0b4b690e5357572baaac
李進興 Vulcan <vulcan@miniasp.com>
2016-10-06 12:50:06 +0800

    Xamarin.Forms 控制項一覽Picker

---
+++ XFPicker/XFPicker/XFPicker/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XFPicker.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
