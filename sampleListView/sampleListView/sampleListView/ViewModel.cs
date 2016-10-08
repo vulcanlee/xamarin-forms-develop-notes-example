977485bec4889f84b2a2804d316160c1816939d4
李進興 Vulcan <vulcan@miniasp.com>
2016-10-08 18:10:56 +0800

    Sample ListView & ObservableCollection

---
+++ sampleListView/sampleListView/sampleListView/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sampleListView
+{
+
+    public class ViewModel
+    {
+        public ObservableCollection<someDataClass> MyItemList { get; set; }
+
+        public ViewModel()
+        {
+            MyItemList = new ObservableCollection<someDataClass>()
+            {
+                new someDataClass { sometext="sometext1", othertext="othertext1" },
+                new someDataClass { sometext="sometext2", othertext="othertext2" },
+                new someDataClass { sometext="sometext3", othertext="othertext3" },
+                new someDataClass { sometext="sometext4", othertext="othertext4" },
+            };
+        }
+    }
+
+    public class someDataClass
+    {
+        public string sometext { get; set; }
+        public string othertext { get; set; }
+    }
+}
