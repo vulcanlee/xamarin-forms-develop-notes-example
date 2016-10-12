508b8a4e01930a8945d5cf3be889cace0a9837bc
李進興 Vulcan <vulcan@miniasp.com>
2016-10-12 16:33:44 +0800

    客製化 Picker 控制項，使其具備可資料綁定能力

---
+++ PickerLab/PickerLab/PickerLab/BindablePicker.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PickerLab
+{
+    // https://forums.xamarin.com/discussion/63565/xamarin-forms-picker-data-binding-using-mvvm
+    public class BindablePicker : Picker
+    {
+        public static readonly BindableProperty ItemsSourceProperty = BindableProperty.Create("ItemsSource",
+                    typeof(IEnumerable), typeof(BindablePicker), null, propertyChanged: OnItemsSourceChanged);
+
+        public static readonly BindableProperty SelectedItemProperty = BindableProperty.Create("SelectedItem",
+                    typeof(IEnumerable), typeof(BindablePicker), null, BindingMode.TwoWay, propertyChanged: OnSelectedItemChanged);
+
+        public static readonly BindableProperty SelectedItemCommandProperty = BindableProperty.Create("SelectedItemCommand", 
+            typeof(ICommand), typeof(BindablePicker), null);
+
+        public BindablePicker()
+        {
+            SelectedIndexChanged += (o, e) =>
+            {
+                if (SelectedIndex < 0 || ItemsSource == null || !ItemsSource.GetEnumerator().MoveNext())
+                {
+                    SelectedItem = null;
+                    return;
+                }
+
+                var index = 0;
+                foreach (var item in ItemsSource)
+                {
+                    if (index == SelectedIndex)
+                    {
+                        SelectedItem = item;
+                        break;
+                    }
+                    index++;
+                }
+            };
+        }
+
+        public ICommand SelectedItemCommand
+        {
+            get { return (ICommand)GetValue(SelectedItemCommandProperty); }
+            set { SetValue(SelectedItemCommandProperty, value); }
+        }
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourceProperty); }
+            set { SetValue(ItemsSourceProperty, value); }
+        }
+
+        public Object SelectedItem
+        {
+            get { return GetValue(SelectedItemProperty); }
+            set
+            {
+                if (SelectedItem != value)
+                {
+                    SetValue(SelectedItemProperty, value);
+                    InternalUpdateSelectedIndex();
+
+                    if(SelectedItemCommand!=null)
+                    {
+                        SelectedItemCommand.Execute(value);
+                    }
+                }
+            }
+        }
+
+        public event EventHandler<SelectedItemChangedEventArgs> ItemSelected;
+
+        private void InternalUpdateSelectedIndex()
+        {
+            var selectedIndex = -1;
+            if (ItemsSource != null)
+            {
+                var index = 0;
+                foreach (var item in ItemsSource)
+                {
+                    if (item != null && item.Equals(SelectedItem))
+                    {
+                        selectedIndex = index;
+                        break;
+                    }
+                    index++;
+                }
+            }
+            SelectedIndex = selectedIndex;
+        }
+
+        private static void OnItemsSourceChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var boundPicker = (BindablePicker)bindable;
+
+            if (Equals(newValue, null) && !Equals(oldValue, null))
+                return;
+
+            boundPicker.Items.Clear();
+
+            if (!Equals(newValue, null))
+            {
+                foreach (var item in (IEnumerable)newValue)
+                    boundPicker.Items.Add(item.ToString());
+            }
+
+            boundPicker.InternalUpdateSelectedIndex();
+        }
+
+        private static void OnSelectedItemChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var boundPicker = (BindablePicker)bindable;
+            if (boundPicker.ItemSelected != null)
+            {
+                boundPicker.ItemSelected(boundPicker, new SelectedItemChangedEventArgs(newValue));
+            }
+            boundPicker.InternalUpdateSelectedIndex();
+        }
+
+    }
+}
---
+++ PickerLab/PickerLab/PickerLab/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,94 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace PickerLab.ViewModels
+{
+
+    
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        #region PickerSelectedTitle
+        private string pickerSelectedTitle;
+        /// <summary>
+        /// PickerSelectedTitle
+        /// </summary>
+        public string PickerSelectedTitle
+        {
+            get { return this.pickerSelectedTitle; }
+            set { this.SetProperty(ref this.pickerSelectedTitle, value); }
+        }
+        #endregion
+
+        #region PickerSelected
+        private string pickerSelected;
+        /// <summary>
+        /// PickerSelected
+        /// </summary>
+        public string PickerSelected
+        {
+            get { return this.pickerSelected; }
+            set { this.SetProperty(ref this.pickerSelected, value); }
+        }
+        #endregion
+
+
+        #region PickerVM
+        private ObservableCollection<string> pickerVM;
+        /// <summary>
+        /// PickerVM
+        /// </summary>
+        public ObservableCollection<string> PickerVM
+        {
+            get { return pickerVM; }
+            set { SetProperty(ref pickerVM, value); }
+        }
+        #endregion
+
+        public DelegateCommand<object> SelectedIndexChangedCommand { get; private set; } 
+
+        public MainPageViewModel()
+        {
+            PickerVM = new ObservableCollection<string>();
+            pickerVM.Add("Item1");
+            pickerVM.Add("Item2");
+            pickerVM.Add("Item3");
+            pickerVM.Add("Item4");
+            pickerVM.Add("Item5");
+            pickerVM.Add("Item6");
+            pickerVM.Add("Item7");
+
+            Title = "這裡是由 SelectedItemCommand 命令觸發來設定";
+            PickerSelectedTitle = "這裡是由 SelectedItem 資料綁定自動設定";
+            SelectedIndexChangedCommand = new DelegateCommand<object>(SelectedIndexChanged);
+        }
+
+        private void SelectedIndexChanged(object obj)
+        {
+            Title = $"已經選擇 {obj}";
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            //if (parameters.ContainsKey("title"))
+            //    Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
