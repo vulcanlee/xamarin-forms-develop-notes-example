6678db0c949de80777c94f2cc6581632171fb6b9
李進興 Vulcan <vulcan@miniasp.com>
2016-10-13 10:58:48 +0800

    顯示具有圓形遮罩 Mask 的圖片

---
+++ ImageCircleLab/ImageCircleLab/ImageCircleLab.Droid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using ImageCircle.Forms.Plugin.Droid;
+
+namespace ImageCircleLab.Droid
+{
+    [Activity(Label = "ImageCircleLab", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.tabs;
+            ToolbarResource = Resource.Layout.toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            ImageCircleRenderer.Init();
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+
+        }
+    }
+}
+
