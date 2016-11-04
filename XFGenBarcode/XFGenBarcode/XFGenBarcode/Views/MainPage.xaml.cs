e52e8f23d9bc2d18902e3c4bbafaaf2bcfdedf6c
李進興 Vulcan <vulcan@miniasp.com>
2016-11-04 13:37:33 +0800

    在 Xamarin.Forms中，使用 ZXing.Net.Mobile 產生各種條碼圖片

---
+++ XFGenBarcode/XFGenBarcode/XFGenBarcode/Views/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace XFGenBarcode.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //var barcode = new ZXingBarcodeImageView
+            //{
+            //    HorizontalOptions = LayoutOptions.FillAndExpand,
+            //    VerticalOptions = LayoutOptions.FillAndExpand,
+            //};
+            //barcode.BarcodeFormat = ZXing.BarcodeFormat.QR_CODE;
+            //barcode.BarcodeOptions.Width = 300;
+            //barcode.BarcodeOptions.Height = 300;
+            //barcode.BarcodeOptions.Margin = 10;
+            //barcode.BarcodeValue = "ZXing.Net.Mobile";
+
+            //stacklayout.Children.Add(barcode);
+        }
+    }
+}
