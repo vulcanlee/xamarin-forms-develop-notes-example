6049cdbbbafeb72a3ec33a432f26b77c2b5dffcf
李進興 Vulcan <vulcan@miniasp.com>
2016-11-06 00:12:28 +0800

    Xamarin.Forms 旋轉木馬 Carousel 控制項效果

---
+++ XFFlip/XFFlip/XFFlip/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using XFFlip.Models;
+
+namespace XFFlip.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #region Zoos
+        private ObservableCollection<Zoo> _Zoos;
+        /// <summary>
+        /// Zoos
+        /// </summary>
+        public ObservableCollection<Zoo> Zoos
+        {
+            get { return _Zoos; }
+            set { SetProperty(ref _Zoos, value); }
+        }
+        #endregion
+
+        public MainPageViewModel()
+        {
+            Zoos = new ObservableCollection<Zoo>
+            {
+                new Zoo
+                {
+                    ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/23c1dd13-333a-459e-9e23-c3784e7cb434/2016-06-02_1049.png",
+                    Name = "Woodland Park Zoo"
+                },
+                 new Zoo
+                {
+                    ImageUrl =    "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/6b60d27e-c1ec-4fe6-bebe-7386d545bb62/2016-06-02_1051.png",
+                    Name = "Cleveland Zoo"
+                 },
+                new Zoo
+                {
+                    ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/e8179889-8189-4acb-bac5-812611199a03/2016-06-02_1053.png",
+                    Name = "Phoenix Zoo"
+                }
+            };
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
