1b1fe7a8e89174ac0f1d7e1ca95349eaba160ab1
李進興 Vulcan <vulcan@miniasp.com>
2016-12-04 00:02:35 +0800

    在 Xamarin.Forms 使用旋轉木馬 CarouselView 控制項，可以左右翻轉

---
+++ XFCarouselView/XFCarouselView/XFCarouselView/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,85 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Forms;
+using XFCarouselView.Models;
+
+namespace XFCarouselView.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        #region Zoos
+        private ObservableCollection<Zoo> _Zoos;
+        /// <summary>
+        /// Zoos
+        /// </summary>
+        public ObservableCollection<Zoo> Zoos
+        {
+            get { return _Zoos; }
+            set { SetProperty(ref _Zoos, value); }
+        }
+        #endregion
+
+        private readonly INavigationService _navigationService;
+
+        public readonly IPageDialogService _dialogService;
+        public MainPageViewModel(INavigationService navigationService, IPageDialogService dialogService)
+        {
+
+            _dialogService = dialogService;
+            _navigationService = navigationService;
+            Zoos = new ObservableCollection<Zoo>
+            {
+                new Zoo
+                {
+                ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/23c1dd13-333a-459e-9e23-c3784e7cb434/2016-06-02_1049.png",
+                Name = "Woodland Park Zoo"
+                },
+                new Zoo
+                {
+                ImageUrl =    "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/6b60d27e-c1ec-4fe6-bebe-7386d545bb62/2016-06-02_1051.png",
+                Name = "Cleveland Zoo"
+                },
+                new Zoo
+                {
+                ImageUrl = "http://content.screencast.com/users/JamesMontemagno/folders/Jing/media/e8179889-8189-4acb-bac5-812611199a03/2016-06-02_1053.png",
+                Name = "Phoenix Zoo"
+                }
+            };
+
+            CarouselView點選Command = new DelegateCommand<SelectedItemChangedEventArgs>( x =>
+            {
+                var foo = x.SelectedItem as Zoo;
+                //await _dialogService.DisplayAlertAsync("Infor", $"Selected {foo.Name}", "OK");
+
+                Title = $"Selected {foo.Name}";
+            });
+        }
+
+        public DelegateCommand<SelectedItemChangedEventArgs> CarouselView點選Command { get; set; }
+
+        public DelegateCommand CarouselViewTest點選Command { get; set; }
+
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+        }
+    }
+}
---
+++ XFCarouselView/XFCarouselView/XFCarouselView/Views/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+using XFCarouselView.Models;
+
+namespace XFCarouselView.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //CarouselZoos.ItemSelected += CarouselZoos_ItemSelected;
+        }
+
+        private void CarouselZoos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            //var foo = e.SelectedItem as Zoo;
+        }
+    }
+}
