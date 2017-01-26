3345d569a42d79822d0a8b0600c18c2bf22b5133
李進興 Vulcan <vulcan@miniasp.com>
2017-01-26 21:07:26 +0800

    如何在 Xamarin.Forms 開發 XAML 用的 Attached Propert

--- AttProp/AttProp/AttProp/EntryTypeAttached.cs
+++ AttProp/AttProp/AttProp/EntryTypeAttached.cs
@@ -7,18 +7,27 @@ using Xamarin.Forms;
 
 namespace AttProp
 {
+    /// <summary>
+    /// 建立一個類別，裡面使用 BindableProperty.CreateAttached 建立一個 XAML 用的附加屬性
+    /// 在 XAML 中的用法     <Entry CustomAttached:EntryTypeAttached.EntryType="Email" />
+    /// </summary>
     public class EntryTypeAttached
     {
+        // 可以使用程式碼片段 vlAttachedProperty 快速產生要設計的程式碼
         #region EntryType Attached Property
+        /// <summary>
+        /// 這個附加屬性，只能夠套用在 Entry 控制項上，會根據所指定的名稱，自動加上浮水印文字
+        /// </summary>
         public static readonly BindableProperty EntryTypeProperty =
                BindableProperty.CreateAttached(
                    propertyName: "EntryType",
                    returnType: typeof(string),
-                   declaringType: typeof(Entry),
-                   defaultValue: null,
-                   defaultBindingMode: BindingMode.OneWay,
+                   declaringType: typeof(Entry),   
+                   defaultValue: null,  // 這個附加屬性的預設值
+                   defaultBindingMode: BindingMode.OneWay,  // 指定資料繫結的模式，在這裡指定為單向
                    validateValue: null,
-                   propertyChanged: OnEntryTypeChanged);
+                   propertyChanged: OnEntryTypeChanged  // 當這個附加屬性值有變動的時候，需要執行的呼叫事件
+                   );
 
         public static void SetEntryType(BindableObject bindable, string entryType)
         {
@@ -30,18 +39,29 @@ namespace AttProp
         }
         #endregion
 
+        /// <summary>
+        /// 當這個附加屬性值有變動的時候，需要執行的呼叫事件
+        /// </summary>
+        /// <param name="bindable"></param>
+        /// <param name="oldValue"></param>
+        /// <param name="newValue"></param>
         private static void OnEntryTypeChanged(BindableObject bindable, object oldValue, object newValue)
         {
+            // 若所附加的屬性不是 Entry，則不要有任何處理動作
             var fooEntry = bindable as Entry;
             if (fooEntry == null)
                 return;
 
+            // 取得屬性值變動前與變動後的值
             var foooldValue = (oldValue as string)?.ToLower();
             var foonewValue = (newValue as string)?.ToLower();
+
             if(foonewValue == null)
             {
                 return;
             }
+
+            #region 根據指定附加屬性的值，設定所綁訂到的控制項(Entry)，的 Placeholder 屬性成為預設指定的文字
             switch (foonewValue)
             {
                 case "None":
@@ -64,8 +84,7 @@ namespace AttProp
                 default:
                     break;
             }
+            #endregion
         }
-
-        // Helper methods for attached bindable property. 
     }
 }
