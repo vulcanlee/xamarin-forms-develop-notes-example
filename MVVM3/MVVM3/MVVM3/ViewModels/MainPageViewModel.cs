58782914481925836d6f72941d90270a1cd4d050
李進興 Vulcan <vulcan@miniasp.com>
2017-01-27 13:26:21 +0800

    使用 Prism 開發框架工具，進行 Xamarin.Forms + MVVM 專案開發

--- MVVM3/MVVM3/MVVM3/ViewModels/MainPageViewModel.cs
+++ MVVM3/MVVM3/MVVM3/ViewModels/MainPageViewModel.cs
@@ -7,6 +7,9 @@ using System.Linq;
 
 namespace MVVM3.ViewModels
 {
+    /// <summary>
+    /// 在這裡，直接使用 Prism 提供的類別 BindableBase，幫助我們處理相關 IPNC 的工作
+    /// </summary>
     public class MainPageViewModel : BindableBase, INavigationAware
     {
         //private string _title;
@@ -27,6 +30,7 @@ namespace MVVM3.ViewModels
             }
             set
             {
+                // SetProperty 是 Prism 所提供的一個方法，讓您不再需要使用 字串 來定義要更新的屬性名稱，並且自動幫您判斷是否要執行更新事件
                 SetProperty(ref _EntryText1, value);
                 if (EntryText1 != EntryText2)
                 {
@@ -84,7 +88,7 @@ namespace MVVM3.ViewModels
             });
         }
 
-
+        #region 這是 Prism 提供另外一個相當好用的方法，用來處理當進入頁面或者來開頁面的時候，需要做哪些事情。要使用這樣功能，您需要實作出 INavigationAware
         public void OnNavigatedFrom(NavigationParameters parameters)
         {
 
@@ -93,5 +97,6 @@ namespace MVVM3.ViewModels
         public void OnNavigatedTo(NavigationParameters parameters)
         {
         }
+        #endregion
     }
 }
