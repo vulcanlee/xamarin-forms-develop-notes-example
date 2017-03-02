06b0aff52265b2d9504118164a13b8f021cdd438
李進興 Vulcan <vulcan@miniasp.com>
2017-03-02 22:48:26 +0800

    在 Xamarin.Forms 中撥放音樂

---
+++ XFSound/XFSound/XFSound/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,44 @@
+﻿using AudioManager;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XFSound.ViewModels
+{
+    public class MainPageViewModel : BindableBase, INavigationAware
+    {
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+
+        public DelegateCommand PlaySoundCommand { get; set; }
+
+
+        public MainPageViewModel()
+        {
+            PlaySoundCommand = new DelegateCommand(async () =>
+           {
+               await Audio.Manager.PlaySound("Drop.mp3");
+           });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("title"))
+                Title = (string)parameters["title"] + " and Prism";
+            await Audio.Manager.PlaySound("Drop.mp3");
+        }
+    }
+}
