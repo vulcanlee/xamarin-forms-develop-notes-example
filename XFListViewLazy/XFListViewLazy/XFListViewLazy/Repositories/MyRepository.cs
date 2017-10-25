651dab4052f698eabfa095b36f49abae37bda6ec
李進興 Vulcan <vulcan@miniasp.com>
2017-10-25 15:56:37 +0800

    Xamarin.Forms 的 ListView 之延遲載入(Lazy Loading) 之設計方法

---
+++ XFListViewLazy/XFListViewLazy/XFListViewLazy/Repositories/MyRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XFListViewLazy.Models;
+
+namespace XFListViewLazy.Repositories
+{
+    public class MyRepository
+    {
+        public const int CycleLength = 20;
+        public List<MyModel> Items = new List<MyModel>();
+        private static MyRepository instance;
+        private MyRepository()
+        {
+            this.GetNext(0);
+        }
+
+        public static MyRepository GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new MyRepository();
+            }
+            return instance;
+        }
+
+        public List<MyModel> GetNext(int lastID)
+        {
+            List<MyModel> foo = new List<MyModel>();
+            for (int i = lastID; i < lastID+MyRepository.CycleLength; i++)
+            {
+                var fooItem = new MyModel()
+                {
+                    ID = i,
+                    Name = $"Name {i}",
+                    Current = DateTime.UtcNow.AddDays(i),
+                };
+                Items.Add(fooItem);
+                foo.Add(fooItem);
+            }
+            return foo;
+        }
+    }
+}
---
+++ XFListViewLazy/XFListViewLazy/XFListViewLazy/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,70 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using Xamarin.Forms;
+using XFListViewLazy.Models;
+using XFListViewLazy.Repositories;
+
+namespace XFListViewLazy.ViewModels
+{
+
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public ObservableCollection<MyModel> MyDatas { get; set; } = new ObservableCollection<MyModel>();
+        public MyModel SelectedMyData { get; set; }
+
+        private readonly INavigationService _navigationService;
+        public MyRepository _myRepository { get; set; }
+
+        public DelegateCommand<MyModel> ItemAppearingCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+            _myRepository = MyRepository.GetInstance();
+
+            ItemAppearingCommand = new DelegateCommand<MyModel>((x) =>
+            {
+                var fooLast = MyDatas.Last();
+                if (x.ID == fooLast.ID)
+                {
+                    Reload(fooLast.ID+1);
+                }
+            });
+        }
+
+        public void OnNavigatedFrom(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationParameters parameters)
+        {
+            Reload(0);
+        }
+
+        public void Reload(int last)
+        {
+            var foo = _myRepository.GetNext(last);
+            foreach (var item in foo)
+            {
+                MyDatas.Add(item);
+            }
+        }
+
+    }
+
+}
