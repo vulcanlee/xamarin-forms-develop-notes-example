dca7d046a1880bad41ebea057de8dfd26d638c11
李進興 Vulcan <vulcan@miniasp.com>
2017-11-15 14:41:32 +0800

    Xamarin.Forms / .NET Standard 體驗之旅 2 : 使用 Acr.UserDialogs 元件

---
+++ NETStdUserDialogs/NETStdUserDialogs/NETStdUserDialogs.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Acr.UserDialogs;
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+
+namespace NETStdUserDialogs.Droid
+{
+    [Activity(Label = "NETStdUserDialogs", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            UserDialogs.Init(this);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App(new AndroidInitializer()));
+        }
+    }
+
+    public class AndroidInitializer : IPlatformInitializer
+    {
+        public void RegisterTypes(IUnityContainer container)
+        {
+            // Register any platform specific implementations
+        }
+    }
+}
+
---
+++ NETStdUserDialogs/NETStdUserDialogs/NETStdUserDialogs/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Acr.UserDialogs;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NETStdUserDialogs.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public DelegateCommand LoadCommand { get; set; }
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+            LoadCommand = new DelegateCommand(async () =>
+            {
+                using (var dlg = UserDialogs.Instance.Loading("Test Progress"))
+                {
+                    await Task.Delay(2000);
+                }
+            });
+        }
+    }
+}
