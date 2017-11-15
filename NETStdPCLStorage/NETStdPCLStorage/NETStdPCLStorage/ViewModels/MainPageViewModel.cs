2828533da93e536081371907d4e35d03e721723c
李進興 Vulcan <vulcan@miniasp.com>
2017-11-15 16:17:16 +0800

    Xamarin.Forms / .NET Standard 體驗之旅 3 : 使用 PCLStorage 元件進行檔案的讀取與寫入

---
+++ NETStdPCLStorage/NETStdPCLStorage/NETStdPCLStorage/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using PCLStorage;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NETStdPCLStorage.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+
+        #region MyEntry
+        private string _MyEntry = "";
+        /// <summary>
+        /// MyEntry
+        /// </summary>
+        public string MyEntry
+        {
+            get { return this._MyEntry; }
+            set { this.SetProperty(ref this._MyEntry, value); }
+        }
+        #endregion
+
+        public DelegateCommand SaveCommand { get; set; }
+        public DelegateCommand CleanCommand { get; set; }
+        public DelegateCommand LoadCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+
+            SaveCommand = new DelegateCommand(async () =>
+            {
+                IFolder rootFolder = FileSystem.Current.LocalStorage;
+                IFolder folder = await rootFolder.CreateFolderAsync("MySubFolder",
+                    CreationCollisionOption.OpenIfExists);
+                IFile file = await folder.CreateFileAsync("answer.txt",
+                    CreationCollisionOption.ReplaceExisting);
+                await file.WriteAllTextAsync(MyEntry);
+            });
+            CleanCommand = new DelegateCommand(() =>
+            {
+                MyEntry = "";
+            });
+            LoadCommand = new DelegateCommand(async () =>
+            {
+                IFolder rootFolder = FileSystem.Current.LocalStorage;
+                IFolder folder = await rootFolder.CreateFolderAsync("MySubFolder",
+                    CreationCollisionOption.OpenIfExists);
+                IFile file = await folder.GetFileAsync("answer.txt");
+                MyEntry = await file.ReadAllTextAsync();
+            });
+        }
+    }
+}
