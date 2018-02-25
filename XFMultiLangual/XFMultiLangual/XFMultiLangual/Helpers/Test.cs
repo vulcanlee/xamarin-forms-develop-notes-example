66e316fc95715f7d59a5f5a894664dcc2fdf83c9
李進興 Vulcan <vulcan@miniasp.com>
2018-02-25 22:53:39 +0800

    Xamarin.Forms / .NET Standard 體驗之旅 6 : 多語應用程式功能設計 - 使用 Multilingual App Toolkit v4.0 (VS 2017)

---
+++ XFMultiLangual/XFMultiLangual/XFMultiLangual/Helpers/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFMultiLangual.Resources
+{
+  public  class Test
+    {
+        public static string MyProperty { get; set; } = "Xamarin";
+    }
+}
---
+++ XFMultiLangual/XFMultiLangual/XFMultiLangual/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+
+namespace XFMultiLangual.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            Title = "Main Page";
+
+            Title = Resources.AppResources.Account;
+        }
+    }
+}
